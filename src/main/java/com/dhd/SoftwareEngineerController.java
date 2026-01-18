0fb5c5499e63b2d665bfe378e9d8d21b3598d8a9
unknown <dhdung71@gmail.com>
2026-01-18 21:12:40 +0700

    basis CRUD

--- src/main/java/com/dhd/SoftwareEngineerController.java
+++ src/main/java/com/dhd/SoftwareEngineerController.java
@@ -1,4 +1,31 @@
 package com.dhd;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
 public class SoftwareEngineerController {
+
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService) {
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers() {
+        return softwareEngineerService.getAllSoftwareEngineers();
+    }
+
+    @GetMapping("{id}")
+    public SoftwareEngineer getEngineerById(@PathVariable Integer id) {
+        return softwareEngineerService.getEngineerById(id);
+    }
+
+    @PostMapping
+    public void addNewSoftwareEngineer(@RequestBody SoftwareEngineer softwareEngineer) {
+        softwareEngineerService.insertSoftwareEngineer(softwareEngineer);
+    }
 }
